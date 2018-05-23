378ea86a0f510cd3493baf4af18fd3749c417ef7
Nancy Jo <enciie@gmail.com>
2018-05-23 21:03:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,15 @@
 class Song
+  attr_accessor :name, :artist, :genre
 
   @@all = []
 
 
-  def self.all? { |e|  }
+  def self.all
+    @@all
+  end
+
+  def initialize(name, artist, genre)
+
+  end
+
 end
