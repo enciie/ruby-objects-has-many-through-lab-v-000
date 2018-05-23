9d7c9c01a34d96eb19117e330290d8c4b9d7ae55
Nancy Jo <enciie@gmail.com>
2018-05-23 21:19:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,6 @@ class Song
 
   @@all = []
 
-
   def self.all
     @@all
   end
@@ -12,6 +11,7 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
+    @@all << self
   end
 
 end
