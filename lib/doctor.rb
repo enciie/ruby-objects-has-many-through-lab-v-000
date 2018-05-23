962f4e201e66cc312c167b5db107c11311b4b9d2
Nancy Jo <enciie@gmail.com>
2018-05-23 21:54:52 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,16 @@
+class Doctor
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(patient, date)
+
+  end
+
+  def appointments
+
+end
