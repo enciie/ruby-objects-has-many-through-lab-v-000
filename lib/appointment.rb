728bb7ceb264f30efed0dff053698ba499aa2ce2
Nancy Jo <enciie@gmail.com>
2018-05-23 22:05:38 +0000

    automatically backed up by learn

---
+++ lib/appointment.rb
@@ -0,0 +1,14 @@
+class Appointment
+  attr_accessor :doctor, :patient,
+
+  @@all = []
+;date
+  def self.all
+    @@all
+  end
+
+  def initialize
+    @date = date
+    @patient = patient
+    @doctor = doctor
+end
